1d781be759525b07d175e01a23a280192cccf564
Francisco <franciscoabreu1408@gmail.com>
2025-12-01 20:00:59 -0300

    Changed dependecies for linux users

--- init_db.py
+++ init_db.py
@@ -14,19 +14,37 @@ def init_db():
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
     DB_PASS = os.getenv("DB_PASS")
+
+
+    print("Connecting to:", DB_HOST, DB_NAME, DB_USER, DB_PASS)
+
     # Conecta-se ao BD
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     cursor = conn.cursor()
+
+    print("Reading schema.sql from:", os.getcwd())
 
     # Lê o SQL file
     with open("schema.sql", "r") as file:
         schema = file.read()
 
-    # Executa os comandos
-    cursor.execute(schema)
-    conn.commit()
+    print("Executing SQL:")
+    print(schema)
+
+    try:
+        # Executa os comandos
+        cursor.execute(schema)
+        conn.commit()
+        print("SUCCESS: Tables created.")
+
+    except Exception as e:
+        print("ERROR executing SQL:", e)
 
     cursor.close()
     conn.close()
 
     print("DB Tables ensured (created if missing)")
+
+
+    if __name__ == "__main__":
+        init_db()
